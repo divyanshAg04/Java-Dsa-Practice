f6d571f30539b1e9af552b20f5b776bb5fe2e9b7
divyanshAg04 <divyanshagrawal536@gmail.com>
2026-06-25 09:56:15 +0530

    findDisappearedNumbers

---
+++ findDisappearedNumbers.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+
+        List<Integer> ans = new ArrayList<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int index = Math.abs(nums[i]) - 1;
+
+            if (nums[index] > 0) {
+                nums[index] = -nums[index];
+            }
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                ans.add(i + 1);
+            }
+        }
+
+        return ans;
+    }
+}
+
+public class findDisappearedNumbers {
+
+    public static void main(String[] args) {
+
+        Solution obj = new Solution();
+
+        int[] nums = {4, 3, 2, 7, 8, 2, 3, 1};
+
+        System.out.println(obj.findDisappearedNumbers(nums));
+    }
+}
