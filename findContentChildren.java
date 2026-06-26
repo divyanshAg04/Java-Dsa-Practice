4c27f39af0665472a0a9ad8375c323c5109d3e3d
divyanshAg04 <divyanshagrawal536@gmail.com>
2026-06-26 10:15:47 +0530

    findContentChildren

---
+++ findContentChildren.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class findContentChildren {
+
+    public static int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int child = 0;
+        int index = 0;
+
+        while (index < s.length && child < g.length) {
+            if (s[index] >= g[child]) {
+                child++;
+            }
+            index++;
+        }
+
+        return child;
+    }
+
+    public static void main(String[] args) {
+
+        int[] g = {1, 2, 3};
+        int[] s = {1, 1};
+
+        System.out.println(findContentChildren(g, s));
+    }
+}
