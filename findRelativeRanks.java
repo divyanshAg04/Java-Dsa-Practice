00edc276fd2c8c674ad20f630e5d32799ff65810
divyanshAg04 <divyanshagrawal536@gmail.com>
2026-07-01 09:26:22 +0530

    findRelativeRanks

---
+++ findRelativeRanks.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class findRelativeRanks {
+
+    public String[] findRelativeRanks(int[] score) {
+
+        Integer[] sortedScore = Arrays.stream(score)
+                .boxed()
+                .sorted(Collections.reverseOrder())
+                .toArray(Integer[]::new);
+
+        String[] medals = {"Gold Medal", "Silver Medal", "Bronze Medal"};
+
+        Map<Integer, String> rankMapping = new HashMap<>();
+
+        for (int i = 0; i < sortedScore.length; i++) {
+            if (i < 3) {
+                rankMapping.put(sortedScore[i], medals[i]);
+            } else {
+                rankMapping.put(sortedScore[i], String.valueOf(i + 1));
+            }
+        }
+
+        String[] result = new String[score.length];
+
+        for (int i = 0; i < score.length; i++) {
+            result[i] = rankMapping.get(score[i]);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        findRelativeRanks obj = new findRelativeRanks();
+
+        int[] score = {10, 3, 8, 9, 4};
+
+        String[] result = obj.findRelativeRanks(score);
+
+        System.out.println("Input Scores: " + Arrays.toString(score));
+        System.out.println("Relative Ranks: " + Arrays.toString(result));
+    }
+}
