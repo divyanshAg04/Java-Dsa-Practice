a08340d381f3cf2a9618a2bb4079c57e739e94b6
divyanshAg04 <divyanshagrawal536@gmail.com>
2026-07-12 07:48:50 +0530

    SameTree

---
+++ SameTree.java
@@ -0,0 +1,52 @@
+// Definition for a binary tree node.
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {}
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+
+        if (p == null || q == null || p.val != q.val) {
+            return false;
+        }
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
+
+public class SameTree {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        // Tree 1
+        TreeNode p = new TreeNode(1);
+        p.left = new TreeNode(2);
+        p.right = new TreeNode(3);
+
+        // Tree 2
+        TreeNode q = new TreeNode(1);
+        q.left = new TreeNode(2);
+        q.right = new TreeNode(3);
+
+        boolean result = sol.isSameTree(p, q);
+
+        System.out.println("Are both trees same? " + result);
+    }
+}
