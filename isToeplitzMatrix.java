5786a841d6063a0c5f0bd52cbbd7032fd797e2fe
divyanshAg04 <divyanshagrawal536@gmail.com>
2026-07-17 08:01:21 +0530

    isToeplitzMatrix

---
+++ isToeplitzMatrix.java
@@ -0,0 +1,24 @@
+public class isToeplitzMatrix {
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[0].length; j++) {
+                if (matrix[i - 1][j - 1] != matrix[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        isToeplitzMatrix obj = new isToeplitzMatrix();
+
+        int[][] matrix = {
+            {1, 2, 3, 4},
+            {5, 1, 2, 3},
+            {9, 5, 1, 2}
+        };
+
+        System.out.println(obj.isToeplitzMatrix(matrix));
+    }
+}
